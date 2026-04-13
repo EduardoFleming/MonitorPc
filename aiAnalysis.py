61231e8d6c7c36f2d4aebeae7ca1df94990e8938
Eduardo Fleming Diniz <eduardo.diniz@tse.jus.br>
2026-04-13 15:50:51 -0300

    Atualizacao automatica - 13/04/2026 15:50:51,59

--- aiAnalysis.py
+++ aiAnalysis.py
@@ -17,6 +17,7 @@ def consultar_gemini(api_key, cpu, gpu, ram, board, disks, uso_principal, uso_de
     3. Gargalo é só se realmente alguma peça atrpalhar o desempenho de outra
     4. Se o nome da placa mãe for genérico de fabricante (ex: HP, Dell, Lenovo), 
     avise que é placa proprietária e coloque na coluna pesquisar como 'N/A'.
+    5. Na tabela, caso a decisão seja de upgrade, na parte de modelo e pesquisas coloque a peça recomendada
 
     COPIE EXATAMENTE ESTE FORMATO PARA A SUA RESPOSTA:
 
@@ -37,12 +38,12 @@ def consultar_gemini(api_key, cpu, gpu, ram, board, disks, uso_principal, uso_de
     🛒 Recomendação: (1 frase curta)
 
     [TABELA]
-    | Tipo de Peça | Modelo | Pesquisar |
+    | Tipo de Peça | Modelo | Decisão | Pesquisar |
     | :--- | :--- | :--- |
-    | Processador | ... (Manter atual ou Upgrade) | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
-    | Placa de Vídeo | ... (Manter atual ou Upgrade) | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
-    | Memória RAM | ... (Manter atual ou Upgrade) | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
-    | Placa-Mãe | ... (Manter atual ou Upgrade) | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
+    | Processador | ... | Manter ou Upgrade | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
+    | Placa de Vídeo | ... | Manter ou Upgrade | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
+    | Memória RAM | ... | Manter ou Upgrade | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
+    | Placa-Mãe | ... | Manter ou Upgrade | [Ver Preço](https://www.google.com/search?tbm=shop&q=...) |
     """
 
     try:
